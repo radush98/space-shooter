9077477800a81bd23a2f83549657fd35b1e5da72
Valdimir Radush <radush9860@gmail.com>
2022-01-13 21:19:02 +0200

    enemies generation and bullet generation

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const startButton = document.querySelector("#start"); //start button
 const gameBlock = document.querySelector('#game');// game button
 const player = document.querySelector('#player');//player's element
 
+const min = 100;
+const max = 500;
+const enemies = { 'type-1': 200, 'type-2': 400 };
+
 /*hide start menu */
 startButton.addEventListener('click', startGame)
 
@@ -30,12 +34,20 @@ document.addEventListener('keydown', event => {
         (event.key == 'ArrowUp' || event.key == 'w')
         && player.offsetTop >= 60
     )
-        player.style.top = `${player.offsetTop - 10}px`
+        player.style.top = `${player.offsetTop - 10}px`;
     if (
         (event.key == 'ArrowDown' || event.key == 's') &&
         player.offsetTop <= (document.querySelector("html").clientHeight - player.clientHeight)
     )
-        player.style.top = `${player.offsetTop + 10}px`
+        player.style.top = `${player.offsetTop + 10}px`;
+    if (
+        event.key == " "
+    ) {
+        const bullet = document.createElement('div');
+        bullet.classList.add('bullet');
+        gameBlock.appendChild(bullet);
+        moveBullet(bullet);
+    }
 })
 
 /*start game function */
@@ -44,30 +56,49 @@ function startGame() {
     startBlock.style.display = 'none';
     gameBlock.style.display = 'block';
 
-    const enemy1 = document.querySelector(".enemy.type-1");
-    setInterval(() => { moveEnemy(enemy1) }, 200);
+    createEnemy();
 
-    const enemy2 = document.querySelector(".enemy.type-2");
-    setInterval(() => { moveEnemy(enemy2) }, 400);
-
-    const bullet = document.querySelector(".bullet");
-    setInterval(() => { moveBullet(bullet) }, 50);
+    // const bullet = document.querySelector(".bullet");
+    // setInterval(() => { moveBullet(bullet) }, 50);
 }
 
 /*enemy move function */
-function moveEnemy(enemy) {
-    enemy.style.left = `${enemy.offsetLeft - 25}px`;
-    if (enemy.offsetLeft < -100) {
-        enemy.style.left = `${document.querySelector("body").clientWidth + 200}px`
-    }
+function moveEnemy(enemy, speed) {
+    const interval = setInterval(() => {
+        enemy.style.left = `${enemy.offsetLeft - 25}px`;
+        if (enemy.offsetLeft < -100) {
+            enemy.remove();
+            createEnemy();
+            clearInterval(interval);
+        }
+    }, speed)
 }
 
 /*move bullet function*/
 function moveBullet(bullet) {
-    bullet.style.left = `${bullet.offsetLeft + 25}px`;
-    if (bullet.offsetLeft > document.body.clientWidth) {
-        bullet.style.left = 130 + "px";
-    }
+    setInterval(() => {
+        bullet.style.left = `${bullet.offsetLeft + 25}px`;
+        if (bullet.offsetLeft > document.body.clientWidth) {
+            bullet.remove();
+        }
+    }, 50);
 }
 
+/*create enemies*/
+function createEnemy() {
+    let position = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    const enemy = document.createElement('div');
 
+    const [enemyType, speed] = getEnemy(); //get our enemy and destruct an array into two constants class and type
+    enemy.classList.add('enemy', enemyType);
+    gameBlock.appendChild(enemy);
+
+    enemy.style.top = `${position}px`;
+    moveEnemy(enemy, speed);
+}
+
+function getEnemy() {
+    const amount = Object.entries(enemies).length - 1;
+    const index = Math.floor(Math.random() * (amount - 0 + 1)) + 0;
+    return Object.entries(enemies)[index];
+}
